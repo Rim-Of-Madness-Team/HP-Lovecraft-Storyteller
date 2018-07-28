9397d7f1362a38195b2118d20dd65fbc463fb86a
jecrell <matt.walls31@gmail.com>
2018-07-28 15:26:02 +0900

    1.0.1977 update

--- Source/HPLovecraft/StorytellerCompProperties_OmenThreatCycle.cs
+++ Source/HPLovecraft/StorytellerCompProperties_OmenThreatCycle.cs
@@ -1,31 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
 using RimWorld;
+using Verse;
 
 namespace HPLovecraft
 {
     public class StorytellerCompProperties_OmenThreatCycle : StorytellerCompProperties
     {
-        public float mtbDaysThreatSmall;
+        
+        public float onDays = 0f;
 
-        public float mtbDaysThreatBig;
+        public float offDays = 0f;
 
-        public float threatOffDays;
+        public float minSpacingDays = 0f;
 
-        public float threatOnDays;
+        public FloatRange numIncidentsRange = FloatRange.Zero;
 
-        public float minDaysBetweenThreatBigs;
+        public bool applyRaidBeaconThreatMtbFactor = false;
 
-        public float ThreatCycleTotalDays
-        {
-            get
-            {
-                return this.threatOffDays + this.threatOnDays;
-            }
-        }
+
+        private IncidentCategoryDef incidentCategory;
+
+        public float forceRaidEnemyBeforeDaysPassed = 0f;
 
         public StorytellerCompProperties_OmenThreatCycle()
         {
             this.compClass = typeof(StorytellerComp_OmenThreatCycle);
         }
+
+        public IncidentCategoryDef IncidentCategory => this.incidentCategory;
+
+        public override IEnumerable<string> ConfigErrors(StorytellerDef parentDef)
+        {
+            if (this.onDays <= 0f)
+            {
+                yield return "onDays must be above zero";
+            }
+            if (this.numIncidentsRange.TrueMax <= 0f)
+            {
+                yield return "numIncidentRange not configured";
+            }
+            if (this.minSpacingDays * this.numIncidentsRange.TrueMax > this.onDays * 0.9f)
+            {
+                yield return "minSpacingDays too high compared to max number of incidents.";
+            }
+            yield break;
+        }
+        
     }
 }
--- Source/HPLovecraft/StorytellerComp_OmenThreatCycle.cs
+++ Source/HPLovecraft/StorytellerComp_OmenThreatCycle.cs
@@ -12,18 +12,12 @@ namespace HPLovecraft
     {
         protected StorytellerCompProperties_OmenThreatCycle Props
         {
-            get
-            {
-                return (StorytellerCompProperties_OmenThreatCycle)this.props;
-            }
+            get { return (StorytellerCompProperties_OmenThreatCycle) this.props; }
         }
 
         protected int QueueIntervalsPassed
         {
-            get
-            {
-                return Find.TickManager.TicksGame / 1000;
-            }
+            get { return Find.TickManager.TicksGame / 1000; }
         }
 
 
@@ -38,64 +32,98 @@ namespace HPLovecraft
         [DebuggerHidden]
         public override IEnumerable<FiringIncident> MakeIntervalIncidents(IIncidentTarget target)
         {
-            float curCycleDays = (GenDate.DaysPassedFloat - this.Props.minDaysPassed) % this.Props.ThreatCycleTotalDays;
-            if (curCycleDays > this.Props.threatOffDays)
+            float difficultyFactor = (!this.Props.applyRaidBeaconThreatMtbFactor)
+                ? 1f
+                : Find.Storyteller.difficulty.raidBeaconThreatCountFactor;
+            int incCount = IncidentCycleUtility.IncidentCountThisInterval(target,
+                Find.Storyteller.storytellerComps.IndexOf(this), this.Props.minDaysPassed, this.Props.onDays,
+                this.Props.offDays, this.Props.minSpacingDays, this.Props.numIncidentsRange.min * difficultyFactor,
+                this.Props.numIncidentsRange.max * difficultyFactor, 1f);
+            for (int i = 0; i < incCount; i++)
             {
-                float daysSinceThreatBig = (float)(Find.TickManager.TicksGame - target.StoryState.LastThreatBigTick) / 60000f;
-                if (daysSinceThreatBig > this.Props.minDaysBetweenThreatBigs && ((daysSinceThreatBig > this.Props.ThreatCycleTotalDays * 0.9f && curCycleDays > this.Props.ThreatCycleTotalDays * 0.95f) || Rand.MTBEventOccurs(this.Props.mtbDaysThreatBig, 60000f, 1000f)))
+                FiringIncident fi = this.GenerateIncident(target);
+                if (fi != null)
                 {
-                    FiringIncident st = this.GenerateQueuedThreatBig(target);
-                    if (st != null)
-                    {
-                        yield return st;
-                    }
-                }
-                
-                if (Rand.MTBEventOccurs(this.Props.mtbDaysThreatSmall, 60000f, 1000f))
-                {
-                    FiringIncident st = this.GenerateQueuedThreatSmall(target);
-                    if (st != null)
-                    {
-                        yield return st;
-                    }
+                    yield return fi;
                 }
             }
+            yield break;
+
+//        (( B18 ))
+//            float curCycleDays = (GenDate.DaysPassedFloat - this.Props.minDaysPassed) % this.Props.ThreatCycleTotalDays;
+//            if (curCycleDays > this.Props.threatOffDays)
+//            {
+//                float daysSinceThreatBig =
+//                    (float) (Find.TickManager.TicksGame - target.StoryState.LastThreatBigTick) / 60000f;
+//                if (daysSinceThreatBig > this.Props.minDaysBetweenThreatBigs &&
+//                    ((daysSinceThreatBig > this.Props.ThreatCycleTotalDays * 0.9f &&
+//                      curCycleDays > this.Props.ThreatCycleTotalDays * 0.95f) ||
+//                     Rand.MTBEventOccurs(this.Props.mtbDaysThreatBig, 60000f, 1000f)))
+//                {
+//                    FiringIncident st = this.GenerateQueuedThreatBig(target);
+//                    if (st != null)
+//                    {
+//                        yield return st;
+//                    }
+//                }
+//
+//                if (Rand.MTBEventOccurs(this.Props.mtbDaysThreatSmall, 60000f, 1000f))
+//                {
+//                    FiringIncident st = this.GenerateQueuedThreatSmall(target);
+//                    if (st != null)
+//                    {
+//                        yield return st;
+//                    }
+//                }
+//            }
         }
-
-        private FiringIncident GenerateQueuedThreatSmall(IIncidentTarget target)
+//
+//        (( B18 ))
+//        private FiringIncident GenerateQueuedThreatSmall(IIncidentTarget target)
+//        {
+//            IncidentDef incidentDef;
+//            if (!this.UsableIncidentsInCategory(IncidentCategoryDefOf.ThreatSmall, target)
+//                .TryRandomElementByWeight(new Func<IncidentDef, float>(base.IncidentChanceFinal), out incidentDef))
+//            {
+//                return null;
+//            }
+//            return new FiringIncident(incidentDef, this, null)
+//            {
+//                parms = this.GenerateParms(incidentDef.category, target)
+//            };
+//        }
+        
+        
+        private FiringIncident GenerateIncident(IIncidentTarget target)
         {
-            IncidentDef incidentDef;
-            if (!this.UsableIncidentsInCategory(IncidentCategoryDefOf.ThreatSmall, target).TryRandomElementByWeight(new Func<IncidentDef, float>(base.IncidentChanceFinal), out incidentDef))
-            {
-                return null;
-            }
-            return new FiringIncident(incidentDef, this, null)
-            {
-                parms = this.GenerateParms(incidentDef.category, target)
-            };
-        }
-
-        private FiringIncident GenerateQueuedThreatBig(IIncidentTarget target, bool isCosmicHorrorEvent=false)
-        {
-            IncidentParms parms = this.GenerateParms(IncidentCategoryDefOf.ThreatBig, target);
-            IncidentDef omenIncident = IncidentDef.Named("HPLovecraft_OmenIncident");
-            if (isCosmicHorrorEvent) omenIncident = IncidentDef.Named("HPLovecraft_OmenIncidentCosmicHorror");
-
-            //Vanilla code
-            if (GenDate.DaysPassed < 20)
-            {
-                if (!IncidentDefOf.RaidEnemy.Worker.CanFireNow(parms))
-                {
-                    return null;
-                }
-            }
-
-            //Throw an omen instead of a big incident
-            return new FiringIncident(omenIncident, this, null)
+            IncidentParms parms = this.GenerateParms(this.Props.IncidentCategory, target);
+            return new FiringIncident(IncidentDef.Named("HPLovecraft_OmenIncident"), this, null)
             {
                 parms = parms
             };
         }
-        
+
+//
+//        private FiringIncident GenerateQueuedThreatBig(IIncidentTarget target, bool isCosmicHorrorEvent = false)
+//        {
+//            IncidentParms parms = this.GenerateParms(IncidentCategoryDefOf.ThreatBig, target);
+//            IncidentDef omenIncident = IncidentDef.Named("HPLovecraft_OmenIncident");
+//            if (isCosmicHorrorEvent) omenIncident = IncidentDef.Named("HPLovecraft_OmenIncidentCosmicHorror");
+//
+//            //Vanilla code
+//            if (GenDate.DaysPassed < 20)
+//            {
+//                if (!IncidentDefOf.RaidEnemy.Worker.CanFireNow(parms))
+//                {
+//                    return null;
+//                }
+//            }
+//
+//            //Throw an omen instead of a big incident
+//            return new FiringIncident(omenIncident, this, null)
+//            {
+//                parms = parms
+//            };
+//        }
     }
 }
